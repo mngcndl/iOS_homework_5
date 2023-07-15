fb854d34c6196f92cf4177b48d2e33f257e4d92a
Liubov Smirnova <83732621+mngcndl@users.noreply.github.com>
2023-07-16 00:03:41 +0300

    the project code

---
+++ homework5_cleaned/homework5_cleaned/CharacterTableViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  CharacterTableViewCell.swift
+//
+
+import UIKit
+
+final class CharacterTableViewCell: UITableViewCell {
+        
+    private lazy var cellNameLabel: UILabel = {
+        let cellNameLabel = UILabel()
+        cellNameLabel.numberOfLines = 0
+        cellNameLabel.textAlignment = .center
+        return cellNameLabel
+    }()
+    
+    private lazy var characterImageView: UIImageView = {
+        let characterImageView = UIImageView()
+        return characterImageView
+    }()
+        
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func setUpData(_ data: Character) {
+        cellNameLabel.text = data.name
+        characterImageView.download(from: data.image)
+    }
+
+
+    
+    func setUpViews() {
+        contentView.addSubview(cellNameLabel)
+        contentView.addSubview(characterImageView)
+        
+        cellNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        characterImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cellNameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.4),
+            cellNameLabel.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.15),
+            cellNameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            cellNameLabel.centerYAnchor.constraint(equalTo: contentView.topAnchor),
+            cellNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            
+            characterImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3),
+            characterImageView.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3),
+            characterImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            characterImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            characterImageView.topAnchor.constraint(equalTo: cellNameLabel.bottomAnchor, constant: 8),
+            ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        cellNameLabel.text = nil
+        characterImageView.image = nil
+    }
+}
